ad24a2aba09d4b350f94062847b1f4b284869020
Harold2011 <haroldmarin2011@gmail.com>
2024-05-07 22:09:35 -0500

    add apariencia de tp

--- resources/views/users/info.blade.php
+++ resources/views/users/info.blade.php
@@ -21,24 +21,35 @@
                 <h2 class="text-xs text-green-500 tracking-widest font-medium title-font mb-1">Tecnoparque Nodo Rionegro</h2>
                 <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">{{ $multimedias->name }}</h1>
                 <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-justify p-20">{{ $multimedias->descripcion }}</p>
-                <div class="w-100px h-50px">
+                <div class="max-w-xl max-h-xl mx-auto">
                     <img class="object-cover object-center rounded" alt="hero" src="{{ asset($multimedias->url) }}">
-                </div>
+                </div>                
             @endforeach
             </div>
             <div class="flex flex-wrap">
-            @foreach ($lineas as $linea)
-            <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">{{ $linea->name }}</h2>
-                <p class="leading-relaxed text-base mb-4 text-justify">{{ $linea->descripcion }}</p>
-                <a class="text-green-500 inline-flex items-center" href="https://redtecnoparque.com/lineas/">Conose mas...
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-                </a>
+                @foreach ($lineas as $linea)
+                <div class="relative xl:w-1/4 lg:w-1/2 max-w-96 sm:mx-auto md:mx-auto md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+                    <div class="overflow-hidden rounded-lg shadow-lg">
+                        <div class="relative z-0">
+                            <img class="w-full h-40 object-cover" src="{{ asset($linea->url) }}" alt="{{ $linea->name }}"> 
+                            <div class="absolute inset-0 bg-black opacity-50"></div> 
+                        </div>
+                        <div class="relative z-10 text-justify bg-white h-[400px] p-2 w-4/5"> 
+                            <h2 class="text-lg sm:text-xl text-gray-800 font-medium title-font mb-2">{{ $linea->name }}</h2>
+                            <p class="leading-relaxed text-gray-600 text-base mb-4">{{ $linea->descripcion }}</p>
+                            <a class="text-green-500 inline-flex items-center" href="https://redtecnoparque.com/lineas/">Conoce más...
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
-            @endforeach
-            </div>
+            
+            
+            
             <div class="flex flex-col text-center w-full mb-20">
                 <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Si tienes una idea, registrala en red tecnoparque colombia.</p>
                 <a href="https://tecnoparque.com.co/registro"><button class="flex mx-auto mt-16 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Registra tu idea</button></a>
