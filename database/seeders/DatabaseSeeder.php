960fb73a284ded9ce7cbeadbe2e301116e81e61e
Harold2011 <haroldmarin2011@gmail.com>
2024-07-08 11:11:04 -0500

    Update Roles, categories and state seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        
+        $this->call([
+            CategorySeeder::class, 
+            AdminUserSeeder::class,
+            StateSeeder::class
         ]);
     }
 }
