5996f3361eb50d6807e4e14811c8f5180f620b53
valencia <147106157+darwinvalencia@users.noreply.github.com>
2024-09-23 11:47:07 -0500

    arreglo nav-user

--- resources/views/admin/like/like.blade.php
+++ resources/views/admin/like/like.blade.php
@@ -6,10 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mis Likes</title>
     <link rel="icon" href="{{ asset('storage/img/logo.jpg') }}" type="image/x-icon">
+    <meta name="author" content="name">
+    <meta name="description" content="description here">
+    <meta name="keywords" content="keywords,here">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js" integrity="sha256-xKeoJ50pzbUGkpQxDYHD7o7hxe0LaOGeguUidbq6vis=" crossorigin="anonymous"></script>
 </head>
 <body class="bg-gray-100 font-sans leading-normal tracking-normal mt-12">
     @include('components.nav-header-dashboard')
